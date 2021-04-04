079cf517ad01c3c5a9890c8776093dca0f2a7cbd
hugooliveira <hugobernardoni@hotmail.com>
2021-04-04 14:53:38 -0300

    Subindo cors

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ require('dotenv').config({
     path: process.env.NODE_ENV === "dev" ? ".env.development" : ".env"
   })
 const express = require('express')
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('OkkkkK');
 })
